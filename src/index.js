fc6870015bcbd450b7c62eb28249ec29e6f2db53
shevkenov <shevkenov@gmail.com>
2020-09-10 11:41:43 +0300

    add REDUX to the project

--- src/index.js
+++ src/index.js
@@ -3,12 +3,29 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import { BrowserRouter as Router } from "react-router-dom";
-import {createStore} from 'redux';
-import {Provider} from 'react-redux';
 
-import reducer from './store/reducer';
+//starting redux
+import { createStore, applyMiddleware, compose, combineReducers } from 'redux';
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
+//ending redux
 
-const store = createStore(reducer);
+//import reducers
+import burgerBuilderReducer from "./store/reducers/burgerBuilder";
+import orderReducer from './store/reducers/order';
+
+//combine reducers
+const rootReducer = combineReducers({
+  burger: burgerBuilderReducer,
+  order: orderReducer
+})
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store = createStore(
+  rootReducer,
+  composeEnhancers(applyMiddleware(thunk))
+);
 
 ReactDOM.render(
   <Provider store={store}>
